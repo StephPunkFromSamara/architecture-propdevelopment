f3d232fd2f7f89a99c13b1c2b916e238e8183450
stepankirpichenkov <skirpichenkov@diasoft.ru>
2025-10-17 23:58:36 +0400

    Проверка задания спринта 5

---
+++ Task5/command-start-service.sh
@@ -0,0 +1,4 @@
+kubectl run front-end-app --image=nginx --labels role=front-end --expose --port 80
+kubectl run back-end-app --image=nginx --labels role=back-end --expose --port 80
+kubectl run admin-back-end-app --image=nginx --labels role=admin-back-end --expose --port 80
+kubectl run admin-front-end-app --image=nginx --labels role=admin-front-end --expose --port 80
